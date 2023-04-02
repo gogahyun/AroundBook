eef8fd5c9ff92a88e6c3449a5394adba943f736e
gogahyun <sspkr1792@naver.com>
2023-04-02 17:19:12 +0900

    프로젝트 생성 및 환경설정

---
+++ AroundBook/src/main/java/com/ggh_dev/AroundBook/AroundBookApplication.java
@@ -0,0 +1,14 @@
+package com.ggh_dev.AroundBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class AroundBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AroundBookApplication.class, args);
+	}
+
+}
---
+++ AroundBook/src/test/java/com/ggh_dev/AroundBook/AroundBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggh_dev.AroundBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AroundBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
